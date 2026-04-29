728ae3d8c6d7f66cd7e4ca18e4c2f2e7eaa0eaa2
escova alisadora <erickcharles8520@gmail.com>
2026-04-29 03:18:03 -0300

    Update viewer.js

--- pages/viewer.js
+++ pages/viewer.js
@@ -11,7 +11,7 @@ export default function Viewer() {
       src={`/pdfs/${file}`}
       width="100%"
       height="100%"
-      style={{ height: "100vh", border: "none" }}
+      style={{ border: "none", height: "100vh" }}
     />
   );
 }
